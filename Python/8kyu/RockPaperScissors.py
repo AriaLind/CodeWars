f724c715e4ef67df3dc6573032e0381829d17e2f
Aria Lind <aria.albin.lind@gmail.com>
2025-05-29 14:22:12 +0200

    New Kata

---
+++ Python/8kyu/RockPaperScissors.py
@@ -0,0 +1,20 @@
+from re import match
+
+def rps (p1, p2):
+
+    if (p1 == p2):
+        return "Draw!"
+
+    match p1:
+        case "rock":
+            if (p2 == "paper"):
+                return "Player 2 won!"
+            return "Player 1 won!"
+        case "paper":
+            if (p2 == "scissors"):
+                return "Player 2 won!"
+            return "Player 1 won!"
+        case "scissors":
+            if (p2 == "rock"):
+                return "Player 2 won!"
+            return "Player 1 won!"
